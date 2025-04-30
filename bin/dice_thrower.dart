d9a183eb792f4d9744391f6fd6c33636ce00efd7
jiri-one <nemec@jiri.one>
2025-04-30 07:47:52 +0200

    switched from print to stdout.writeln and format output

--- bin/dice_thrower.dart
+++ bin/dice_thrower.dart
@@ -16,18 +16,18 @@ void main(List<String> arguments) {
   final ArgResults arg = parser.parse(arguments);
   if (arg.arguments.contains("-h")
       || arg.arguments.contains("--help")) {
-    print('Usage:\ndice_thrower dice/s\ndice_thrower [options]\n');
-    print("Dice/s are numbers that represent the number of faces on the die. If you want more dice, separate each die with a space.\n");
-    print('Options:');
-    print('  -h, --help     Show this help message');
-    print('  -v, --version  Show version information');
-    print("\n");
+    stdout.writeln('Usage:\ndice_thrower dice/s\ndice_thrower [options]\n');
+    stdout.writeln("Dice/s are numbers that represent the number of faces on the die. If you want more dice, separate each die with a space.\n");
+    stdout.writeln('Options:');
+    stdout.writeln('  -h, --help     Show this help message');
+    stdout.writeln('  -v, --version  Show version information');
+    stdout.writeln("\n");
   }
   
   if (arg.arguments.contains("-v")
              || arg.arguments.contains("--version")) {
-    print('dice_thrower version 0.0.1');
-    print("\n");
+    stdout.writeln('dice_thrower version 0.0.1');
+    stdout.writeln("\n");
   }
 
   final argumentsList = arg.rest;
@@ -41,8 +41,8 @@ List<int> inputChecker(List<String> arguments) {
   for (var dice in arguments) {
     // check if dice is a number
     if (int.tryParse(dice) == null) {
-      print('Invalid argument: $dice');
-      print('Please provide a list of integers.');
+      stdout.writeln('Invalid argument: $dice');
+      stdout.writeln('Please provide a list of integers.');
       exitCode = 1;
       return [];
     }
@@ -53,8 +53,8 @@ List<int> inputChecker(List<String> arguments) {
   for (int dice in diceList) {
     // check if dice is greater than 1
     if (dice <= 1) {
-      print('Invalid argument: $dice');
-      print('Please provide a list of integers greater than 1.');
+      stdout.writeln('Invalid argument: $dice');
+      stdout.writeln('Please provide a list of integers greater than 1.');
       exitCode = 1;
       return [];
     }
@@ -68,7 +68,7 @@ List<int> randomizer(List<int> diceList) {
   List<int> dicesValueList = [];
   for (int dice in diceList) {
     int randomNumber = random.nextInt(dice) + 1;
-    print('Rolling a $dice sided die: $randomNumber');
+    stdout.writeln('\n Rolling a $dice sided die: $randomNumber');
     dicesValueList.add(randomNumber);
   }
   return dicesValueList;
